0c6a957531cadd3a34c971e002a9ffbcc3507847
YauheniLinkevich <yauheni.linkevich@gmail.com>
2021-01-05 23:21:13 +0300

    added operations with -42 and -15

--- src/HomeWork1/Task1.java
+++ src/HomeWork1/Task1.java
@@ -63,5 +63,67 @@ public class Task1 {
         // <<= 42 << 1: 0010 1010 -> 0101 0100; 15 << 1: 0000 1111 -> 0011 1100
         firstNumber <<= 1;
         secondNumber <<= 2;
+
+        /// operations with numbers -42 and -15
+        // ~ -42: 1101 0110 -> 0010 1001; -15: 1111 0001 -> 0000 1110
+        firstNumber = -42;
+        secondNumber = -15;
+        result = ~firstNumber;
+        result = ~secondNumber;
+
+        // & result == -48: 1101 0000
+        result = firstNumber & secondNumber;
+
+        // &= -42: 1101 0110 -> 1101 0000; -15: 1111 0001 -> 1101 0000
+        firstNumber &= secondNumber;
+        firstNumber = -42;
+        secondNumber &= firstNumber;
+        secondNumber = -15;
+
+        // | result == -9: 1111 0111
+        result = firstNumber | secondNumber;
+
+        // |= -42: 1101 0110 -> 1111 0111; -15: 1111 0001 -> 1111 0111
+        firstNumber |= secondNumber;
+        firstNumber = -42;
+        secondNumber |= firstNumber;
+        secondNumber = -15;
+
+        // ^ result == 39: 0010 0111
+        result = firstNumber ^ secondNumber;
+
+        // ^= -42: 1101 0110 -> 0010 0111; -15: 1111 0001 -> 0010 0111
+        firstNumber ^= secondNumber;
+        firstNumber = -42;
+        secondNumber ^= firstNumber;
+        secondNumber = -15;
+
+        // >> -42 >> 4: 1101 0110 -> 1111 1101; -15 >> 2: 1111 0001 -> 1111 1100
+        result = firstNumber >> 4;
+        result = secondNumber >> 2;
+
+        // >>= -42 >> 2: 1101 0110 -> 1111 0101; -15 >> 3: 1111 0001 -> 1111 1110
+        firstNumber >>= 2;
+        secondNumber >>= 3;
+        firstNumber = -42;
+        secondNumber = -15;
+
+        // >>> -42 >>> 3: 1101 0110 -> 00011111111111111111111111111010; -15 >>> 2: 1111 0001 -> 00111111111111111111111111111100
+        result = firstNumber >>> 3;
+        result = secondNumber >>> 2;
+
+        // >>>= -42 >>> 3: 1101 0110 -> 00011111111111111111111111111010; -15 >>> 2: 1111 0001 -> 00111111111111111111111111111100
+        firstNumber >>>= 3;
+        secondNumber >>>= 2;
+        firstNumber = -42;
+        secondNumber = -15;
+
+        // << -42 << 2: 1101 0110 -> 0101 1000; -15 << 4: 1111 0001 -> 0001 0000
+        result = firstNumber << 2;
+        result = secondNumber << 4;
+
+        // <<= -42 << 1: 1101 0110 -> 1010 1100; -15 << 1: 1111 0001 -> 1100 0100
+        firstNumber <<= 1;
+        secondNumber <<= 2;
     }
 }
